f045408e4eb35830aafcd611f6872232d7ccf020
duela <duelapower@gmail.com>
2023-07-13 02:57:36 +0300

    Implement Level 2 Database encryption

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const bodyParser = require("body-parser");
 const _ = require('lodash');
 const port = 3000;
 const app = express();
+const uri = require(__dirname + "/uri.js");
+var encrypt = require('mongoose-encryption');
 
 //Listening on port 3000 and if it goes well then logging a message saying that the server is running
 app.listen(process.env.PORT || port, function(req, res){
@@ -21,11 +23,18 @@ app.use(express.static("public")); // use to store static files like images css
 
 mongoose.connect('mongodb://127.0.0.1:27017/userDB'); // Start and connect to mongodb server
 
+// Level 2 security
+
 // Create a user Schema
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+// It is important that you add this plugin to the schema before you create the Mongoose model because the userSchema is passed as a parameter to create the new Mongoose model
+const secret = uri.secret(); // or a unique key can be use as a form of string
+//userSchema.plugin(encrypt, { secret: secret, excludeFromEncryption: ['email'] }); // This encrypt the entire database and exclude "email"
+//userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password', 'socialNumber', 'creditCard'] }); // This encrypt only the "['password', 'socialNumber', 'creditCard']" in the entire database
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] }); // This encrypt only the "password" in the entire database
 
 // Create user Model
 const User = mongoose.model("User", userSchema);
@@ -61,6 +70,12 @@ app.route('/register')
 
 app.route('/login')
 .get(function(req, res) {
+    // User.find({}).then(function(loginGet){
+    //   console.log(loginGet);
+    //   res.send(loginGet);
+    // }).catch(function(err){
+    //   console.log(err);
+    // });
   res.render('login');
 })
 .post(function(req, res) {
@@ -72,7 +87,8 @@ app.route('/login')
     else {
       if (loginPost) {
         if (loginPost.password === req.body.password) {
-          res.render("secrets")
+
+          res.render("secrets");
         }
         else {
           res.render('error', {error: "Invalid password, try again!"});
@@ -85,12 +101,69 @@ app.route('/login')
 });
 
 
-
-
-
-// app.get('/secrets', function(req, res) {
-//   res.render('secrets');
+// Level 1 Authentication and security
+//
+// // Create a user Schema
+// const userSchema = new Schema({
+//     email: String,
+//     password: String
 // });
-// app.get('/submit', function(req, res) {
-//   res.render('submit');
+//
+// // Create user Model
+// const User = mongoose.model("User", userSchema);
+//
+//
+// app.get('/', function(req, res) {
+//   res.render('home');
 // });
+// // using express app.route(). chainable route to reduce redundacy and typos
+// app.route('/register')
+// .get(function(req, res) {
+//   res.render('register');
+// })
+// .post(function(req, res) {
+//   User.findOne({email: req.body.username}).then(function(registerPost){
+//     if (!registerPost) {
+//       const newUser =  new User({
+//         email: req.body.username,
+//         password: req.body.password
+//       });
+//       newUser.save();
+//       res.render('secrets');
+//     }
+//     else {
+//       const errorMessage = "Oops! " +req.body.username + " already exist!"
+//       res.render('error', {error: errorMessage});
+//     }
+//   }).catch(function(err) {
+//     console.log(err);
+//     res.redirect('/');
+//   });
+// });
+//
+// app.route('/login')
+// .get(function(req, res) {
+//   res.render('login');
+// })
+// .post(function(req, res) {
+//   User.findOne({email: req.body.username})
+//   .then(function(loginPost){
+//     if (!loginPost) {
+//       console.log("No account");
+//     }
+//     else {
+//       if (loginPost) {
+//         if (loginPost.password === req.body.password) {
+//           res.render("secrets")
+//         }
+//         else {
+//           res.render('error', {error: "Invalid password, try again!"});
+//         }
+//       }
+//     }
+//   }).catch(function(err) {
+//     console.log(err);
+//   });
+// });
+// .render('submit');
+// // });
